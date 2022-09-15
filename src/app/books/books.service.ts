ce3c557ed4faddd760311320507ffd07ab2b30ed
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-15 14:31:54 +0300

    tailwind

--- src/app/books/books.service.ts
+++ src/app/books/books.service.ts
@@ -23,6 +23,18 @@ export class BooksService {
         author: "David Thomas",
         image: "https://m.media-amazon.com/images/I/51W1sBPO7tL._AC_SY780_.jpg",
         amount: 800
+      },
+      {
+        name: 'Clean Code',
+        author: "Robert C Martin",
+        image: "https://images-na.ssl-images-amazon.com/images/I/41BsaF+yiSL._SX373_BO1,204,203,200_.jpg",
+        amount: 700,
+      },
+      {
+        name: "The Pragmatic Programmer",
+        author: "David Thomas",
+        image: "https://m.media-amazon.com/images/I/51W1sBPO7tL._AC_SY780_.jpg",
+        amount: 800
       }
     ];
   }
