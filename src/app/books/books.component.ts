554296ef992f5bf06c85aa64f0eba9992f742ea2
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-15 10:40:14 +0300

    pipe kullanıımı

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -11,12 +11,14 @@ export class BooksComponent implements OnInit {
     {
       name: 'Clean Code',
       author: "Robert C Martin",
-      image: "https://images-na.ssl-images-amazon.com/images/I/41BsaF+yiSL._SX373_BO1,204,203,200_.jpg"
+      image: "https://images-na.ssl-images-amazon.com/images/I/41BsaF+yiSL._SX373_BO1,204,203,200_.jpg",
+      amount: 700,
     },
     {
       name: "The Pragmatic Programmer",
       author: "David Thomas",
-      image: "https://m.media-amazon.com/images/I/51W1sBPO7tL._AC_SY780_.jpg"
+      image: "https://m.media-amazon.com/images/I/51W1sBPO7tL._AC_SY780_.jpg",
+      amount: 800
     }
   ]
 
@@ -43,4 +45,5 @@ interface Book {
   name: string
   author: string
   image: string
+  amount: number
 }
