6d903749be2ce7d6c7c84a094432fe05cabfb87d
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-15 17:15:07 +0300

    firebase register

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginForm } from 'src/app/types/Auth';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 @Component({
   selector: 'app-register',
@@ -20,8 +21,24 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  passwordMatched: boolean = true;
 
   submit(): void {
+    if (this.form.password !== this.form.confirm_password) {
+      this.passwordMatched = false;
+      return;
+    }
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, this.form.email, this.form.password).then(
+      (userCredentials) => {
+        console.log(userCredentials);
+        const user = userCredentials.user;
+      }
+    ).catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    })
+
 
   }
 
