c889bfbc8da1a54bc9ab15ff05af2d9acd7b2914
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-15 19:13:50 +0300

    Create firebase.config.ts

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "yourApiKEY",
+    authDomain: "yourDomain",
+    projectId: "yourProjectId",
+    storageBucket: "yourStorageBucket",
+    messagingSenderId: "yourMessagingSender",
+    appId: "yourAppId"
+};
+
